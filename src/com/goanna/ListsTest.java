855f32eeec0cc9aa4e8de616d3904dbbcd147566
indiefernando <indie.fernando@Outlook.com>
2021-07-16 11:54:57 +1000

    Quiz submission

--- src/com/goanna/ListsTest.java
+++ src/com/goanna/ListsTest.java
@@ -10,29 +10,84 @@ import org.junit.jupiter.api.Test;
 
 class ListsTest {
 	
-	@BeforeEach
-	void Initialize() {
+	/*
+	 * @BeforeEach void Initialize() { Employee emp1 = new Employee(1, "Mohit", 22);
+	 * Employee emp2 = new Employee(2, "Rahul", 42); Employee emp3 = new Employee(3,
+	 * "Shyam", 24); Employee emp4 = new Employee(4, "Mohit", 36); Employee emp5 =
+	 * new Employee(5, "Swaraj", 60);
+	 * 
+	 * 
+	 * List<Employee> empList = new ArrayList<>(10);
+	 * 
+	 * empList.add(emp1); empList.add(emp2); empList.add(emp3); empList.add(emp4);
+	 * empList.add(emp5);
+	 * 
+	 * }
+	 */
+
+
+	@Test
+	void testOver60() {
+		
 		Employee emp1 = new Employee(1, "Mohit", 22);
         Employee emp2 = new Employee(2, "Rahul", 42);
         Employee emp3 = new Employee(3, "Shyam", 24);
         Employee emp4 = new Employee(4, "Mohit", 36);
         Employee emp5 = new Employee(5, "Swaraj", 60);
-
-        
-        List<Employee> empList = new ArrayList<>(10);
+		
+		List<Employee> empList = new ArrayList<>(10);
 
         empList.add(emp1);
         empList.add(emp2);
         empList.add(emp3);
         empList.add(emp4);
         empList.add(emp5);
-
-	}
-
-
-	@Test
-	void test() {
 		
+		Lists.over60(empList);
+		
+		assertEquals(null, Lists.over60(empList));
+	}
+	
+	@Test
+	void testRemoveById() {
+		
+		Employee emp1 = new Employee(1, "Mohit", 22);
+        Employee emp2 = new Employee(2, "Rahul", 42);
+        Employee emp3 = new Employee(3, "Shyam", 24);
+        Employee emp4 = new Employee(4, "Mohit", 36);
+        Employee emp5 = new Employee(5, "Swaraj", 60);
+		
+		List<Employee> empList = new ArrayList<>(10);
+
+        empList.add(emp1);
+        empList.add(emp2);
+        empList.add(emp3);
+        empList.add(emp4);
+        empList.add(emp5);
+		
+		Lists.removeById(empList, 4);
+		
+		assertEquals(4, empList.size());
+	}
+	
+	@Test
+	void testAgeModulus() {
+		
+		Employee emp1 = new Employee(1, "Mohit", 22);
+        Employee emp2 = new Employee(2, "Rahul", 42);
+        Employee emp3 = new Employee(3, "Shyam", 24);
+        Employee emp4 = new Employee(4, "Mohit", 36);
+        Employee emp5 = new Employee(5, "Swaraj", 60);
+		
+		List<Employee> empList = new ArrayList<>(10);
+
+        empList.add(emp1);
+        empList.add(emp2);
+        empList.add(emp3);
+        empList.add(emp4);
+        empList.add(emp5);
+		
+		assertTrue(Lists.ageModulus(empList));
 	}
 
 }
